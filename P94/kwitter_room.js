3306db89b1634ef8977b4af5b9a545ac1bb2aec7
Bhumika-patil4747 <83863445+Bhumika-patil4747@users.noreply.github.com>
2021-09-08 16:52:27 +0530

    Add files via upload

---
+++ P94/kwitter_room.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7iKIvRVd52ZKi4YwRffLptmbDiIqNGUY",
+    authDomain: "p-continue.firebaseapp.com",
+    projectId: "p-continue",
+    storageBucket: "p-continue.appspot.com",
+    messagingSenderId: "69417629291",
+    appId: "1:69417629291:web:104f6cb6377a5bd7e5232e"
+  };
